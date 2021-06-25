fdaedfc993c5859ab663f16f82a229d8a81c318e
shurashovD <78542938+shurashovD@users.noreply.github.com>
2021-06-25 17:30:27 +0300

    123

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.get('*', (req, res) => {
 });
 
 if ( process.env.NODE_ENV === 'production' ) {
+    console.log('Production');
     app.use('/', express.static(path.join(__dirname, 'client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
