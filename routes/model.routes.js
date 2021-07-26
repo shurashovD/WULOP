412c55a3cb15e0209869a420acb7ce5d31f1a3cb
Дмитрий Шурашов <shurashovd@yandex.ru>
2021-07-26 19:33:50 +0300

    Почта отключена

--- routes/model.routes.js
+++ routes/model.routes.js
@@ -3,34 +3,8 @@ const AuthMiddleWare = require('../middleware/auth.middleware');
 const {check, validationResult} = require('express-validator');
 const Model = require('../models/Model');
 const Profile = require('../models/Profile');
-const nodeFetch = require('node-fetch');
-const nodemailer = require('nodemailer');
 const path = require('path');
 const config = require('config');
-
-async function mail() {
-    const result = await nodeFetch('https://oauth.yandex.ru/authorize?response_type=code&client_id=16c29b63eb3740bfa539a47781793ccb')
-    console.log(result);
-/*
-    const transporter = nodemailer.createTransport({
-        host: "smtp.yandex.ru",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            type: 'OAuth2',
-            user: 'knaub.sabina@yandex.ru',
-            accessToken: 'AQAAAABW3wlBAAdFWuV2737reE6apaZrGrCVxUg'
-        }
-    });
-    
-    const info = await transporter.sendMail({
-        from: 'knaub.sabina@yandex.ru', // sender address
-        to: "shurashovd@yandex.ru", // list of receivers
-        subject: "Test", // Subject line
-        text: "Hello world?" // plain text body
-    });
-    console.log(info);*/
-}
 
 const router = Router();
 
@@ -198,17 +172,6 @@ router.post(
 
             if ( model.scores.length == 2 ) {
                 model.completed = true;
-                if ( model.mail ) {
-                    const transporter = nodemailer.createTransport(config.smtp);
-                    const info = await transporter.sendMail({
-                        from: '"Fred Foo 👻" <shurashovd@yandex.ru>', // sender address
-                        to: "shurashovd@yandex.ru", // list of receivers
-                        subject: "Hello ✔", // Subject line
-                        text: "Hello world?", // plain text body
-                        html: "<b>Hello world?</b>", // html body
-                    });
-                    console.log(info);
-                }
             }
 
             await model.save();
@@ -299,7 +262,5 @@ router.post(
         }
     }
 );
-
-mail();
 
 module.exports = router;
